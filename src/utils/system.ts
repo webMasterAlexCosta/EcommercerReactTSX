67a922dee8485ae25ec241fcb7029fde1473feb5
Alex Costam <thg6321@gmail.com>
2025-03-09 15:49:40 -0300

    mudanca de dominio backend

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,7 +1,7 @@
 
 
 export const BASE_URL_LOCAL2="http://localhost:8080"
-export const BASE_URL_LOCAL="https://exceptional-cathi-alevivaldi-fe38a61b.koyeb.app"
+export const BASE_URL_LOCAL="https://quaint-adele-alevivaldi-a5632bd1.koyeb.app/"
 
 
 export const storageCarrinho = "br.com.alexcosta/carrinho"
