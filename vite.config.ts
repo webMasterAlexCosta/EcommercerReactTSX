4b85bf18ca0f748bdc102fbcd54f6c0b4bcf899f
Alex Costam <thg6321@gmail.com>
2025-03-12 16:18:54 -0300

    corrigindo yarn.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { BASE_URL_LOCAL } from './src/utils/system';
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
   },
   server: {
-    historyApiFallback: true
-  }
+    proxy: {
+      // Redirecionando todas as requisições para /api para o backend Java Spring Boot na porta 8080
+      '/api': {
+        target: BASE_URL_LOCAL, // URL do seu backend
+        changeOrigin: true, // Pode ser necessário para garantir que as cabeçalhos de CORS funcionem corretamente
+        rewrite: (path) => path.replace(/^\/api/, ''), // Se necessário, reescreve o caminho, removendo "/api"
+      },
+    },
+    fs: {
+      strict: false, // Evitar erros com arquivos durante o desenvolvimento
+    },
+  },
 });
