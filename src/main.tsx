96946ae17e9ebf868c997a8f3c7f0f61ddad2267
Alex Costam <thg6321@gmail.com>
2025-03-28 02:41:22 -0300

    revertendo a otimizacao main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,7 @@
-import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { Carregando } from './components/UI/Carregando';
-import { TEXTO_PADRAO_SOLICITACAO } from './utils/system';
-
-const LazyApp = React.lazy(() => import('./App'));
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
-  <Suspense fallback={<div><Carregando title={TEXTO_PADRAO_SOLICITACAO}/></div>}>
-    <LazyApp />
-  </Suspense>
+  <App />
 );
